c4a57b405d75365328c366eafcebb5be620a7c33
Matt Cappleman <matt.cappleman@gmail.com>
2017-12-08 10:50:37 -0500

    Adding in the config variables per environment

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
+"""Imports"""
+import os
 from flask import Flask
-app = Flask(__name__)
 
-@app.route('/')
+APP = Flask(__name__)
+APP.config.from_object(os.environ['APP_SETTINGS'])
+
+@APP.route('/')
 def hello():
+    """Return Hello World"""
     return "Hello World!"
 
+@APP.route('/<name>')
+def hello_name(name):
+    """Return Hello World"""
+    return "Hello {}!".format(name)
+
 if __name__ == '__main__':
-    app.run()
+    APP.run()
---
+++ config.py
@@ -0,0 +1,33 @@
+"""Import OS"""
+import os
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    """Base Config to be inherited from"""
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'this-really-needs-to-be-changed'
+
+
+class ProductionConfig(Config):
+    """Production Config variables go here"""
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    """Staging Config variables go here"""
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    """Development Config variables go here"""
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    """Testing Config variables go here"""
+    TESTING = True
